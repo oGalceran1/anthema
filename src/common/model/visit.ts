4428e38e791217c8e83f28d86731efa24299bd78
alfonsserra <alfonsoserra@gmail.com>
2019-01-17 17:35:13 +0100

    Visit empty constructor

--- src/common/model/visit.ts
+++ src/common/model/visit.ts
@@ -2,7 +2,7 @@ import { Patient } from '@model/patient';
 
 export class Visit {
 
-	constructor(public id: string, public date: Date, public patient: Patient, public location: string) {
+	constructor(public id?: string, public date?: Date, public patient?: Patient, public location?: string) {
 	}
 }
 
